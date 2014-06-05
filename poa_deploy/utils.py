ebfcb5f56a3bea44d02a25d18daf584c993184e2
vmedvedev <fknd99@gmail.com>
2014-06-05 07:25:28 +0700

    Added cleanup

--- poa_deploy/utils.py
+++ poa_deploy/utils.py
@@ -3,6 +3,8 @@ import xmlrpclib
 import socket
 import urllib2
 import xml.etree.ElementTree as ET
+from poa_deploy.models import Instance
+
 #import logging
 
 
@@ -49,7 +51,7 @@ def get_application_mandatory_settings(app_meta_parsed):
 
     mandatory_settings_ids_list = [setting.attrib['id'] for setting in app_meta_parsed.findall(".//{http://apstandard.com/ns/1}setting")
                                    if 'default-value' not in setting.attrib and 'min-length' in setting.attrib]
-    random_value = '1qazXSW@'
+    random_value = 'ADMINPASSWORD4'
     return [{'name': setting_id, 'value': random_value} for setting_id in mandatory_settings_ids_list]
 
 def connect_via_rpc(mn_ip):
@@ -104,3 +106,57 @@ def provide_application_instance(sub_id, rt_id, domain_name, api, settings):
               'settings': settings,
               'txn_id': api.txn_id }
     return api.execute('pem.APS.provideApplicationInstance', **params)
+
+def fully_provide_application(application, api):
+    app_name = application.app_name
+    app_url = application.url
+    app_meta_url = get_appmeta_url(app_url)
+    app_meta_parsed = get_app_meta_parsed(app_meta_url)
+    application_settings = get_application_mandatory_settings(app_meta_parsed)
+    #import_response = import_app_to_poa(app_url, api)
+    #poa_app_id = import_response.get('application_id')
+    poa_app_id = 204
+    rt_response = create_rt_for_app(poa_app_id, app_name, api)
+    rt_id = rt_response.get('resource_type_id')
+    st_id = create_service_template(rt_id, app_name, api)
+    subscr_response = create_subscription(st_id, app_name, api)
+    sub_id = subscr_response.get('subscription_id')
+    domain_name = subscr_response.get('domain_name')
+    instance_response = provide_application_instance(sub_id, rt_id,
+                             domain_name, api, application_settings)
+    instance_id = instance_response.get('application_instance_id')
+    instance_params = {'st_id': st_id,
+                       'rt_id': rt_id,
+                       'poa_app_id': poa_app_id,
+                       'sub_id': sub_id,
+                       'instance_id' : instance_id,
+                       'domain_name': domain_name}
+    return instance_params
+
+def cleanup_instance(instance, api):
+    remove_sub(instance.sub_id, api)
+    remove_domain(instance.domain_name, api)
+    remove_st(instance.st_id, api)
+    remove_rt(instance.rt_id, api)
+    remove_application_from_poa(instance.poa_app_id)
+
+def remove_sub(sub_id, api):
+    params = {'subscription_id': sub_id}
+    api.execute('pem.removeSubscription', **params)
+    
+def remove_domain(domain_name, domain_id=None, api):
+    params = {'domain_name': domain_name} if domain_name is None\
+        else {'domain_id': domain_id}
+    api.execute('pem.removeDomain', **params)
+
+def remove_st(st_id, api):
+    params = {'st_id': st_id}
+    api.execute('pem.removeServiceTemplate', **params)
+
+def remove_rt(rt_id, api):
+    params = {'rt_id': rt_id}
+    api.execute('pem.removeResourceType', **params)
+
+def remove_application_from_poa(app_id, api):
+    params = {'application_id': app_id}
+    api.execute('pem.APS.removeApplication', **params)
